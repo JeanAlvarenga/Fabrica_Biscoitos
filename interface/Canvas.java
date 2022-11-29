94384c750ea665437f49a3359c06236b6764c64c
Jean Alvarenga <62890014+JeanAlvarenga@users.noreply.github.com>
2022-11-29 11:33:37 -0300

    Imagem de Fundo adicionada

---
+++ interface/Canvas.java
@@ -0,0 +1,23 @@
+import javax.swing.JPanel;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Canvas extends JPanel {
+    BufferedImage image;
+
+    public Canvas(){
+        try{
+            image = ImageIO.read(new File("D:\\documentos\\GitHub\\Fabrica_Biscoitos\\imagens\\fundo1.png"));
+        }catch(Exception e){
+            System.out.println("Erro ao carregar imagem");
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image.getScaledInstance(874, 629, 0), 0, 0, null);
+    }
+}
--- interface/Interface.java
+++ interface/Interface.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 public class Interface extends JFrame implements ActionListener {
 	// Atributos
     //RESERVA DE MEMÓRIA E CRIAÇÃO DA VARIAVEL "pedido" PARA ARMAZENAR fila DE PEDIDOS
@@ -16,49 +15,50 @@ public class Interface extends JFrame implements ActionListener {
 
     // Icone das imagens no jogo
     JFrame janela = new JFrame("Add request"); // Cria a janela com o titulo "Add request".
-    private ImageIcon imFundo = new ImageIcon(getClass().getResource("fundo.png"));
-    private JLabel fundo = new JLabel(imFundo);
-    private int janelaW = 874, janelaH = 629;
 
-    JLabel one = new JLabel("Ingredient 1"); // Cria o label "Ingredient 1".
-	JLabel two = new JLabel("Ingredient 2"); // Cria o label "Ingredient 2".
-	JLabel three = new JLabel("Ingredient 3"); // Cria o label "Ingredient 3".
+	private ImageIcon imGP = new ImageIcon(getClass().getResource("GotaPreta.png"));
+	private JLabel GP = new JLabel(imGP);
 
-	JTextField primeiro = new JTextField(10);
-	JTextField segundo = new JTextField(10);
-	JTextField terceiro = new JTextField(10);
+    private JLabel one = new JLabel("Ingredient 1"); // Cria o label "Ingredient 1".
+	private JLabel two = new JLabel("Ingredient 2"); // Cria o label "Ingredient 2".
+	private JLabel three = new JLabel("Ingredient 3"); // Cria o label "Ingredient 3".
 
-	String[] tipos = { "Comum", "Recheado"};
-	JComboBox<String> listaTiposBiscoitos = new JComboBox<>(tipos); // Cria a lista de tipos de biscoitos.
+	private JTextField primeiro = new JTextField(10);
+	private JTextField segundo = new JTextField(10);
+	private JTextField terceiro = new JTextField(10);
 
-	JButton botao1 = new JButton(" Add request "); // Cria o botão com o texto " Adicionar pedido ".
-	JButton botao2 = new JButton(" Start process "); // Cria o botão com o texto " iniciar processo ".
-	JLabel texTamanhoFila = new JLabel(" Queue size: ");
-	JLabel tamFila = new JLabel("           null          "); // Cria o label "null".
-		
+	private String[] tipos = { "Comum", "Recheado"};
+	private JComboBox<String> listaTiposBiscoitos = new JComboBox<>(tipos); // Cria a lista de tipos de biscoitos.
 
-    //Quando clicamos no botão é executad o método addBiscoito da classe Pedido.
-	//botao1.addActionListener(new AcaoBotao(listaTiposBiscoitos, primeiro, segundo, terceiro, tamFila));
+	private JButton botao1 = new JButton(" Add request "); // Cria o botão com o texto " Adicionar pedido ".
+	private JButton botao2 = new JButton(" Start process "); // Cria o botão com o texto " iniciar processo ".
+	private JLabel texTamanhoFila = new JLabel(" Queue size: ");
+	private JLabel tamFila = new JLabel("           null          "); // Cria o label "null".
+	
+	JPanel painel = new JPanel(); // Cria o painel.
+	Canvas canvas = new Canvas();
     
     public void desenharGraficos() {
-        JPanel painel = new JPanel();
-        painel.setSize(janelaW, janelaH);
-		painel.add(listaTiposBiscoitos);
-		painel.add(one);
-		painel.add(primeiro);
-		painel.add(new JLabel("  "));
-		painel.add(two);
-		painel.add(segundo);
-		painel.add(new JLabel("  "));
-		painel.add(three);
-		painel.add(terceiro);
-		painel.add(botao1);
-		painel.add(texTamanhoFila);
-		painel.add(tamFila);
-		painel.add(botao2);
-        painel.add(fundo);
-        janela.setSize(janelaW, janelaH);
-        janela.add(painel);
+		
+		// Configurações da janela
+		canvas.add(listaTiposBiscoitos);
+		canvas.add(one);
+		canvas.add(primeiro);
+		canvas.add(new JLabel("  "));
+		canvas.add(two);
+		canvas.add(segundo);
+		canvas.add(new JLabel("  "));
+		canvas.add(three);
+		canvas.add(terceiro);
+		canvas.add(botao1);
+		canvas.add(texTamanhoFila);
+		canvas.add(tamFila);
+		canvas.add(botao2);
+
+		canvas.add(GP);
+		GP.setBounds(110,60,imGP.getIconWidth(),imGP.getIconHeight());
+
+		janela.add(canvas);
 		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		janela.pack();
 		janela.setVisible(true);
@@ -109,9 +109,17 @@ public class Interface extends JFrame implements ActionListener {
 		}
 	}
 
+	public void exibirGotaPreta() {
+		GP.setBounds(110,60,imGP.getIconWidth(),imGP.getIconHeight());
+		canvas.add(GP);
+		
+		//janela.setVisible(true);
+	}
+
     public void run(){
         desenharGraficos();
         botao();
+		//exibirGotaPreta();
         
     }
 }
